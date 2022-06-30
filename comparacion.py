195938dc0aae639063de73f3fe4d61e1a7e75ca8
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-06-30 16:09:01 -0500

    Operadores de Comparación

---
+++ comparacion.py
@@ -0,0 +1,12 @@
+print("Ingresa tu edad")
+edad = int(input())
+
+""" if edad >= 18:
+  print("Eres Mayor de Edad")
+else:
+  print("Eres Menor de Edad") """
+
+if edad < 18:
+  print("Eres Menor de Edad")
+else:
+  print("Eres Mayor de Edad")
